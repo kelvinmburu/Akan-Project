bc0a88970341501285497d19fed4f3247e3b0dee
kelvinmburu <mburuhkelvin@gmail.com>
2022-03-07 04:01:20 +0300

    ft: added var for dob and month

--- js/app.js
+++ js/app.js
@@ -1,26 +1,22 @@
 function getAkanName() {
-    let birthYear = document.getElementById("numYear").value;
-    let birthMonth = document.getElementById("numMonth").value;
-    let birthDate = document.getElementById("numDay").value;
+    let birthMonth = document.form.numMonth.selectedIndex;
+    let birthMonth = document.form.month.options[birthMonth].value;
+    let birthDate = document.form.numDay.selectedIndex;
+    let birthDate = document.form.options[numDay].value;
+    let birthYear = document.form.numYear.value;
+    //Gender variables declaration
+    let mlGender = document.form.maleGender.selectedIndex;
+    let mlGender = document.form.options[maleGender].value;
+    let feGender = docment.form.femaleGender.selectedIndex;
+    let feGender = document.form.options[femaleGender].value;
 
-    let genders = document.getElementsByName("gender");
-
-    //Syntax to display gender
-
-    function getGender() {
-        for (let gender of genders) {
-            if (gender.checked) {
-                return gender.value;
-            }
-        }
-    }
-    let myGenderValue = getGender();
-
-    console.log(myGenderValue);
-
-    //Function to validate month and year
-
-    function validateMonth() {
-
-    }
 }
+
+// Date array declaration and container
+
+var dOB = month + "" + birthDate + ", " + birthYear;
+var whenBorn = new Date(dOB);
+var akanDay = whenBorn.getDate();
+var date = whenborn.getDate();
+var yearOB = whenborn.getFullYear();
+
