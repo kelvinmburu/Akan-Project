c241098b7a199d2bf024fb055c4cd11efebaf77c
kelvinmburu <mburuhkelvin@gmail.com>
2022-03-07 04:57:37 +0300

    st: beautified, idented, and formatted code

--- js/app.js
+++ js/app.js
@@ -10,79 +10,80 @@ function getAkanName() {
     let feGender = docment.form.femaleGender.selectedIndex;
     let feGender = document.form.options[femaleGender].value;
 
-// Date array declaration and container
+    // Date array declaration and container
 
-var dOB = month + "" + birthDate + ", " + birthYear;
-var whenBorn = new Date(dOB);
-var akanDay = whenBorn.getDate();
-var date = whenBorn.getDate();
-var yearOB = whenBorn.getFullYear();
+    var dOB = month + "" + birthDate + ", " + birthYear;
+    var whenBorn = new Date(dOB);
+    var akanDay = whenBorn.getDate();
+    var date = whenBorn.getDate();
+    var yearOB = whenBorn.getFullYear();
 
-// Syntax for invalid date, birth month, and yearOB
+    // Syntax for invalid date, birth month, and yearOB
 
-if (date <= 0 || date >= 31) {
-    alert("Invalid birth date")
-}
+    if (date <= 0 || date >= 31) {
+        alert("Invalid birth date")
+    }
 
-if (birthMonth <= 0 || birthMonth >= 12) {
-    alert("Invalid birth month")
-}
-
-
-if (yearOB < 100) yearOB = "19" + whenBorn.getFullYear();
-else year = whenBorn.getFullYear();
-
-//Syntax displays the days of the week contained in a variable
-
-var weekDays = new Array(6);
-
-weekDays[0] = "Sunday";
-weekDays[1] = "Monday";
-weekDays[2] = "Tuesday";
-weekDays[3] = "Wednesday";
-weekDays[4] = "Thursday";
-weekDays[5] = "Friday";
-weekDays[6] = "Saturday";
-
-// Variable containing Akan name display
-
-var akanFemale = new Array(6);
-
-akanFemale[0] = "Akosua";
-akanFemale[1] = "Adwoa";
-akanFemale[2] = "Abenaa";
-akanFemale[3] = "Akua";
-akanFemale[4] = "Yaa";
-akanFemale[5] = "Afua";
-akanFemale[6] = "Ama";
-
-// Variable containing Akan male names display
-
-var akanMale = new Array(6);
-
-akanFemale[0] = "Kwasi";
-akanFemale[1] = "Kwadwo";
-akanFemale[2] = "Kwabena";
-akanFemale[3] = "Kwaku";
-akanFemale[4] = "Yaw";
-akanFemale[5] = "Kofi";
-akanFemale[6] = "Kwame";
-
-// If function for male and female names generation
-
-if (mlGender == "male"){
-    generateName = akanMale[akanDay]
-}
-else {generateName = akanFemale[akanDay]};
+    if (birthMonth <= 0 || birthMonth >= 12) {
+        alert("Invalid birth month")
+    }
 
 
-// Function for female name generation
+    if (yearOB < 100) yearOB = "19" + whenBorn.getFullYear();
+    else year = whenBorn.getFullYear();
 
-if (birthDate != date) alert("Invalid date!");
-else {
-    dateBorn = generateName;
-    dateWeek = weekDays[akanDay];
-    dOB = dateBorn + ", " + "born on a" + " " + dateWeek;
-    alert("Great! Your Akan name is " + dOB);
-}
+    //Syntax displays the days of the week contained in a variable
+
+    var weekDays = new Array(6);
+
+    weekDays[0] = "Sunday";
+    weekDays[1] = "Monday";
+    weekDays[2] = "Tuesday";
+    weekDays[3] = "Wednesday";
+    weekDays[4] = "Thursday";
+    weekDays[5] = "Friday";
+    weekDays[6] = "Saturday";
+
+    // Variable containing Akan name display
+
+    var akanFemale = new Array(6);
+
+    akanFemale[0] = "Akosua";
+    akanFemale[1] = "Adwoa";
+    akanFemale[2] = "Abenaa";
+    akanFemale[3] = "Akua";
+    akanFemale[4] = "Yaa";
+    akanFemale[5] = "Afua";
+    akanFemale[6] = "Ama";
+
+    // Variable containing Akan male names display
+
+    var akanMale = new Array(6);
+
+    akanFemale[0] = "Kwasi";
+    akanFemale[1] = "Kwadwo";
+    akanFemale[2] = "Kwabena";
+    akanFemale[3] = "Kwaku";
+    akanFemale[4] = "Yaw";
+    akanFemale[5] = "Kofi";
+    akanFemale[6] = "Kwame";
+
+    // If function for male and female names generation
+
+    if (mlGender == "male") {
+        generateName = akanMale[akanDay]
+    } else {
+        generateName = akanFemale[akanDay]
+    };
+
+
+    // Function for female name generation
+
+    if (birthDate != date) alert("Invalid date!");
+    else {
+        dateBorn = generateName;
+        dateWeek = weekDays[akanDay];
+        dOB = dateBorn + ", " + "born on a" + " " + dateWeek;
+        alert("Great! Your Akan name is " + dOB);
+    }
 }
