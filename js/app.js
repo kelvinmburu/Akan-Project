99a9400b474fc85db6e7b883c93183359edd02b6
kelvinmburu <mburuhkelvin@gmail.com>
2022-03-07 04:13:41 +0300

    beautified code and added akan and female names

--- js/app.js
+++ js/app.js
@@ -20,3 +20,52 @@ var akanDay = whenBorn.getDate();
 var date = whenborn.getDate();
 var yearOB = whenborn.getFullYear();
 
+// Syntax for invalid date, birth month, and yearOB
+
+if (birthDate <= 0 || birthDate >= 31) {
+    alert("Invalid birth date")
+}
+
+if (birthMonth <= 0 || birthMonth >= 12) {
+    alert("Invalid birth month")
+}
+
+
+if (birthYear < 100) birthYear = "19" + whenborn.getFullYear();
+else year = whenborn.getFullYear();
+
+//Syntax displays the days of the week contained in a variable
+
+var weekDays = new Array(6);
+
+weekDays[0] = "Sunday";
+weekDays[1] = "Monday";
+weekDays[2] = "Tuesday";
+weekDays[3] = "Wednesday";
+weekDays[4] = "Thursday";
+weekDays[5] = "Friday";
+weekDays[6] = "Saturday";
+
+// Variable containing Akan name display
+
+var akanFemale = new Array(6);
+
+akanFemale[0] = "Akosua";
+akanFemale[1] = "Adwoa";
+akanFemale[2] = "Abenaa";
+akanFemale[3] = "Akua";
+akanFemale[4] = "Yaa";
+akanFemale[5] = "Afua";
+akanFemale[6] = "Ama";
+
+// Variable containing Akan male names display
+
+var akanMale = new Array(6);
+
+akanFemale[0] = "Akosua";
+akanFemale[1] = "Adwoa";
+akanFemale[2] = "Abenaa";
+akanFemale[3] = "Akua";
+akanFemale[4] = "Yaa";
+akanFemale[5] = "Afua";
+akanFemale[6] = "Ama";
